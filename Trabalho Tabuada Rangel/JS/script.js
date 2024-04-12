e5ec9a626e8a76e2ca035e76692fbfd23f1b98ac
Edimar Gabriel Marques <139916604+GabrielMarques1@users.noreply.github.com>
2024-04-12 01:45:20 -0300

    Add files via upload

---
+++ Trabalho Tabuada Rangel/JS/script.js
@@ -0,0 +1,255 @@
+//Prefixo EGM VEM DE EDIMAR GABRIEL MARQUES
+
+
+const EGM_numero_1 = document.getElementById('um').addEventListener('click', function(){
+    let EGM_numero = 1;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_2 = document.getElementById('dois').addEventListener('click', function(){
+    let EGM_numero = 2;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_3 = document.getElementById('tres').addEventListener('click', function(){
+    let EGM_numero = 3;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_4 = document.getElementById('quatro').addEventListener('click', function(){
+    let EGM_numero = 4;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_5 = document.getElementById('cinco').addEventListener('click', function(){
+    let EGM_numero = 5;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_6 = document.getElementById('seis').addEventListener('click', function(){
+    let EGM_numero = 6;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_7 = document.getElementById('sete').addEventListener('click', function(){
+    let EGM_numero = 7;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_8 = document.getElementById('oito').addEventListener('click', function(){
+    let EGM_numero = 8;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
+const EGM_numero_9 = document.getElementById('nove').addEventListener('click', function(){
+    let EGM_numero = 9;
+    let EGM_tabuada = '';
+    let add_evento_operaçao = document.getElementById('add').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} + ${i} = ${EGM_numero + i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+        }});
+     let sub_evento_operaçao = document.getElementById('substract').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} - ${i} = ${EGM_numero - i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let mult_evento_operaçao = document.getElementById('multiply').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';} 
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} x ${i} = ${EGM_numero * i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});
+     let div_evento_operaçao = document.getElementById('divide').addEventListener('click', function(){
+        if(EGM_tabuada != ''){EGM_tabuada = '';}
+            for(let i = 1; i <= 10; i++){
+                EGM_tabuada += `${EGM_numero} / ${i} = ${EGM_numero / i}<br>`;
+                resultado.innerHTML = EGM_tabuada;
+            }});            
+});
